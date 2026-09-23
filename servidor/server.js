0d8e92cfd606f0ebe9a36d44173542043f04759f
Samuka9191 <sahesa91@gmail.com>
2026-09-23 10:45:25 -0300

    att

---
+++ servidor/server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const inventario = require("../dados.json");
+
+const mostrarInventario = (req, res) => {
+    res.send(inventario)
+}
+
+const novoInventario = (req, res) => {
+    if (req.body) {
+        res.send("Novo Inventario recebido!")
+        inventario.push(req.body)
+    } else {
+        res.send("Erro ao receber pedido no estoque!")
+    }
+}
+
+const excluirInventario = (req, res) => {
+    const id = req.params.id;
+
+    inventario.forEach((inventarios, indice) => {
+        if(inventarios.id == id){
+            inventario.splice(indice, 1);
+        }
+    });
+
+    res.send("Inventario Excluido do estoque com sucesso!")
+};
+
+const alterarInventario = (req, res) => {
+    const id = req.params.id;
+    const dados = req.body;
+
+    inventario.forEach((inventarios) => {
+        if(inventarios.id == id) {
+            inventarios.item = dados.item;
+            inventarios.local = dados.local;
+            inventarios.dataRegistro = dados.dataRegistro;
+            inventarios.valor = dados.valor;
+            inventarios.patrimonio = dados.patrimonio;
+        }
+    });
+    res.send("Pedido atulizado com sucesso");
+};
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+const porta = 3000;
+
+app.get("/", mostrarInventario);
+app.post("/", novoInventario);
+app.delete("/:id", excluirInventario);
+app.put("/:id", alterarInventario);
+
+app.listen(porta, () => {
+console.log(`Servidor: http://127.0.0.1:${porta}`);
+});
